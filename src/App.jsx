cc86a3ba705866e14eb69a6d6df43dd26ec35db5
kevinhartzog12 <kevinhartzog12@gmail.com>
2026-01-11 12:06:57 -0800

    Added Firebase database stuff

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,24 @@ import {
   LayoutDashboard
 } from 'lucide-react';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 // --- Mock Data ---
 const MOCK_VOTES = [
   { 
